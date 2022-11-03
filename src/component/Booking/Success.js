029b87acc71043eab7252599a3f651ae6b6c10d8
Sagar Lade <81896221+sagarlade@users.noreply.github.com>
2022-11-03 06:47:13 -0700

    Update Success.js

--- src/component/Booking/Success.js
+++ src/component/Booking/Success.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './style.css'
 const Success = () => {
   return (
     <div>
-      <h1>You are done!</h1>
+      <h1 className="sucess">You are done!</h1>
     </div>
   )
 }
